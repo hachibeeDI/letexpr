d7f31dbbbc64338787755d0d88159171f6f5eccc
dogura <daiki.ogura@soliton.co.jp>
2014-01-28 13:54:23 +0900

    support lazy evalucation

--- letexpr.py
+++ letexpr.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+from __future__ import (print_function, division, absolute_import, unicode_literals,)
+
 
 from inspect import getargspec
 
@@ -31,7 +33,8 @@ class let(object):
         ''' :type action: (str, func) '''
         name, f = action
         require_arg_keys = getargspec(f).args
-        self.lets[name] = f(
+        self.lets[name] = MemoizedLazyFunction(
+            f,
             **self.__extract_require_args(require_arg_keys)
         )
         return self
@@ -42,13 +45,25 @@ class let(object):
             binded_items = self.lets.iteritems()
         except AttributeError:
             binded_items = self.lets.items()
-        return dict((k, v) for k, v in binded_items if k in arg_keys)
+        return dict((k, func()) for k, func in binded_items if k in arg_keys)
 
     def in_(self, func):
         require_arg_keys = getargspec(func).args
         return func(
             **self.__extract_require_args(require_arg_keys)
         )
+
+
+class MemoizedLazyFunction(object):
+    def __init__(self, func, **require_args):
+        self.func = func
+        self.args = require_args
+        self.result = None  # Option?
+
+    def __call__(self):
+        if self.result is None:
+            self.result = self.func(**self.args)
+        return self.result
 
 
 def _test():
--- tests.py
+++ tests.py
@@ -1,6 +1,14 @@
 # -*- coding: utf-8 -*-
+from __future__ import (print_function, division, absolute_import, unicode_literals,)
 
+import sys
 import unittest
+if sys.version_info[0] == 2:
+    from itertools import izip_longest as zip_longest
+else:
+    from itertools import zip_longest
+
+
 from letexpr import let
 
 from nose.tools import eq_
@@ -12,6 +20,39 @@ loader = unittest.TestLoader()
 # suite.addTests(doctest.DocTestSuite(letexpr))
 
 
-def test_initialize_with_func():
+def test_executed():
     (let() | ('x', lambda: 10)
      ).in_(lambda x: eq_(x, 10))
+
+
+def test_lazy_evalucated():
+    '''
+    The let-expr module evalucate the expression is demanded as arguments.
+    And content of that expression would not be evalucated.
+    '''
+    watcher = LoadOrderWatcher()
+    (let()
+     | ('x', lambda: watcher.append('Iam x'))
+     | ('y', lambda: watcher.append('Iam y'))
+     | ('z', lambda y: watcher.append('Iam z'))
+     | ('_a', lambda x: watcher.append('Iam _a'))
+     ).in_(lambda _a, z: None)
+    watcher.expected(
+        'Iam y',
+        'Iam x',
+        'Iam z',
+        'Iam _a',
+    )
+
+
+class LoadOrderWatcher(list):
+    def __init__(self):
+        super(LoadOrderWatcher, self).__init__()
+
+    def append(self, var):
+        super(LoadOrderWatcher, self).append(var)
+        return var
+
+    def expected(self, *orders):
+        for expect, supplied in zip_longest(orders, self):
+            eq_(expect, supplied)
